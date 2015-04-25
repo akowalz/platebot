43c86c146ed67fd1b193bdc227362bcf18a836a6
Alex Kowalczuk <askowalczuk93@gmail.com>
2015-04-25 17:48:15 -0500

    fix migration type error 'date_time'

--- db/migrate/20150423212541_remove_column_from_late_plate.rb
+++ db/migrate/20150423212541_remove_column_from_late_plate.rb
@@ -1,6 +1,6 @@
 class RemoveColumnFromLatePlate < ActiveRecord::Migration
   def change
     remove_column :late_plates, :when
-    add_column :late_plates, :t, :date_time
+    add_column :late_plates, :t, :datetime
   end
 end
